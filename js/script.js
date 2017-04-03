ae5df8bfc0c5da5c8b13e91f1f1b16bd48afbf63
serhii-kalyna <sergii_kalyna@mail.ru>
2017-04-03 17:28:19 +0300

    7

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/*$(function(){
+
+	$('#carousel').elastislide();
+
+});
+
+$.Elastislide.defaults = {
+	// orientation 'horizontal' || 'vertical'
+	orientation : 'horizontal',
+
+	// sliding speed
+	speed : 500,
+
+	// sliding easing
+	easing : 'ease-in-out',
+
+	// the minimum number of items to show. 
+	// when we resize the window, this will make sure minItems are always shown 
+	// (unless of course minItems is higher than the total number of elements)
+	minItems : 3,
+	
+	// index of the current item (left most item of the carousel)
+	start : 0,
+	
+	// click item callback
+	onClick : function( el, position, evt ) { return false; },
+	onReady : function() { return false; },
+	onBeforeSlide : function() { return false; },
+	onAfterSlide : function() { return false; }
+};*/
